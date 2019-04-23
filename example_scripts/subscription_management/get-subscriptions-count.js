a4e5a6b9961a94fbbfd3c2b67ab1d9ff3a137a77
Mustafa K. Isik <mustafa.isik@kerngedanke.com>
2019-04-23 10:29:41 +0200

    URL to Twitter API endpoint seems to have changed
    fixed the url path to include `subscriptions`

--- example_scripts/subscription_management/get-subscriptions-count.js
+++ example_scripts/subscription_management/get-subscriptions-count.js
@@ -6,7 +6,7 @@ auth.get_twitter_bearer_token().then(function (bearer_token) {
 
   // request options
   var request_options = {
-    url: 'https://api.twitter.com/1.1/account_activity/all/count.json',
+    url: 'https://api.twitter.com/1.1/account_activity/all/subscriptions/count.json',
     auth: {
       'bearer': bearer_token
     }
